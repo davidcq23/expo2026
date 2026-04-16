30a31454d22971ebfcce46d82c7496c5a2d79d43
davidcq23 <42897015+davidcq23@users.noreply.github.com>
2026-04-16 11:24:02 -0600

    Add files via upload

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -7,28 +7,24 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
 {
     public DbSet<Contact> Contacts => Set<Contact>();
 
-   protected override void OnModelCreating(ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.Entity<Contact>(entity =>
         {
-            entity.ToTable("contacts");
-
             entity.HasKey(x => x.Id);
-
-            entity.Property(x => x.Id).HasColumnName("id");
-            entity.Property(x => x.Nombre).HasColumnName("nombre").IsRequired().HasMaxLength(100);
-            entity.Property(x => x.Apellidos).HasColumnName("apellidos").IsRequired().HasMaxLength(150);
-            entity.Property(x => x.Telefono).HasColumnName("telefono").IsRequired().HasMaxLength(30);
-            entity.Property(x => x.Correo).HasColumnName("correo").IsRequired().HasMaxLength(150);
-            entity.Property(x => x.Empresa).HasColumnName("empresa").HasMaxLength(150);
-            entity.Property(x => x.Codigo).HasColumnName("codigo").IsRequired().HasMaxLength(50);
-            entity.Property(x => x.UsoCodigo).HasColumnName("uso_codigo").HasDefaultValue(false);
-            entity.Property(x => x.CreatedAtUtc).HasColumnName("created_at").HasDefaultValueSql("CURRENT_TIMESTAMP");
-
             entity.HasIndex(x => x.Correo).IsUnique();
             entity.HasIndex(x => x.Codigo).IsUnique();
+
+            entity.Property(x => x.Nombre).IsRequired().HasMaxLength(100);
+            entity.Property(x => x.Apellidos).IsRequired().HasMaxLength(150);
+            entity.Property(x => x.Telefono).IsRequired().HasMaxLength(30);
+            entity.Property(x => x.Correo).IsRequired().HasMaxLength(150);
+            entity.Property(x => x.Empresa).HasMaxLength(150);
+            entity.Property(x => x.Codigo).IsRequired().HasMaxLength(50);
+            entity.Property(x => x.UsoCodigo).HasDefaultValue(false);
+            entity.Property(x => x.CreatedAtUtc).HasDefaultValueSql("CURRENT_TIMESTAMP");
         });
     }
 }
